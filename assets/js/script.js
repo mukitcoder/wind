0c9f59a99684e25b2c6d0b4e396d0b3c887b9b83
mukitcoder <mukitbd71@gmail.com>
2023-03-02 16:35:50 +0600

    create megamenu

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,14 @@
 	"use strict";
 
 
+	$(document).ready(function () {
+		$("ul.navbar-nav > li").click(function (e) {
+		 $("ul.navbar-nav > li").removeClass("active");
+		 $(this).addClass("active");
+		  });
+	  });
+
+
 	$(function() {  
 		$('.btn-1')
 		.on('mouseenter', function(e) {
